db26816ae05d68f5efee613e0d6cbd0aedb1e946
Evalley1995 <evalley.felipe@gmail.com>
2025-06-02 23:59:07 -0400

    Add files via upload

---
+++ Exp1_S1_Felipe_Hidalgo_Alan_Figueroa_Grupo10/src/exp1_s2_felipe_hidalgo_alan_figueroa_grupo10/Cliente.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exp1_s1_felipe_hidalgo_alan_figueroa_grupo10;
+
+/**
+ *
+ * @author pipe-
+ */
+// Clase Cliente
+class Cliente {
+    private String rut;
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    String domicilio;
+    String comuna;
+    String telefono;
+   
+
+    public Cliente(String rut, String nombre, String apellidoPaterno, String apellidoMaterno,
+                   String domicilio, String comuna, String telefono) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.domicilio = domicilio;
+        this.comuna = comuna;
+        this.telefono = telefono;
+     
+    }
+
+    public void mostrarDatos() {
+        System.out.println("RUT: " + rut);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido Paterno: " + apellidoPaterno);
+        System.out.println("Apellido Materno: " + apellidoMaterno);
+        System.out.println("Domicilio: " + domicilio);
+        System.out.println("Comuna: " + comuna);
+        System.out.println("Teléfono: " + telefono);
+
+    }
+
+   
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "rut=" + rut + ", nombre=" + nombre + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno=" + apellidoMaterno + ", domicilio=" + domicilio + ", comuna=" + comuna + ", telefono=" + telefono + '}';
+    }
+
+    
+    
+}
